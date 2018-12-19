ce9e5b56a12656cca3de6e309ad74d195d0d2bc2
Sam Bennrubi <sbennrubi@gmail.com>
2018-12-19 00:22:46 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,9 @@
 class CashRegister
-  attr_accessor :discount, :total
+  attr_accessor :discount, :total, :items
   
   def initialize(discount = 0)
     @total = 0
     @discount = discount
-  
   end
   
   def add_item(item, price, quantity = 1)
@@ -19,5 +18,6 @@ class CashRegister
       "There is no discount to apply."
     end
   end
+
 
 end
